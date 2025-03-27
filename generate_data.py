5a1e8d941f768ebf141f5dd52e44156a3a457f6e
Andy-Borch <64490941+Andy-Borch@users.noreply.github.com>
2025-03-27 11:56:24 -0600

    Added datatypes to add to csv

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,4 @@
-#Generate arrays, arraylists, tuples, etc
+#Generate lists, tuples, linkedLists
 #Put into csv file
 #include size, number of unique elements, etc
 #maybe create a function that determines initial sortedness without sorting
