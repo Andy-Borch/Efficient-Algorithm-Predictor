d086602ea1a9caf741f9feaed9c679a1b69b4a17
Andy-Borch <64490941+Andy-Borch@users.noreply.github.com>
2025-03-27 11:44:39 -0600

    Added task to generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,4 @@
 #Generate arrays, arraylists, tuples, etc
 #Put into csv file
 #include size, number of unique elements, etc
+#maybe create a function that determines initial sortedness without sorting
